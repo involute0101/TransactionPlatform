d2bf9145dcfe35c37219e6d882a4f52c0d36b307
wzk <1403235458@qq.com>
2021-05-21 09:32:17 +0800

    登陆注册、忘记密码

---
+++ LoginRegister/LoginRegister/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace LoginRegister
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelLogin = new System.Windows.Forms.Panel();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.buttonRegister = new System.Windows.Forms.Button();
+            this.buttonForget = new System.Windows.Forms.Button();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.panelLogin.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelLogin
+            // 
+            this.panelLogin.Controls.Add(this.textBoxPassword);
+            this.panelLogin.Controls.Add(this.textBoxUsername);
+            this.panelLogin.Controls.Add(this.label2);
+            this.panelLogin.Controls.Add(this.usernameLabel);
+            this.panelLogin.Controls.Add(this.buttonLogin);
+            this.panelLogin.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogin.Location = new System.Drawing.Point(0, 0);
+            this.panelLogin.Name = "panelLogin";
+            this.panelLogin.Size = new System.Drawing.Size(466, 441);
+            this.panelLogin.TabIndex = 0;
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonLogin.Location = new System.Drawing.Point(199, 268);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogin.TabIndex = 0;
+            this.buttonLogin.Text = "登录";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            // 
+            // buttonRegister
+            // 
+            this.buttonRegister.Location = new System.Drawing.Point(0, 454);
+            this.buttonRegister.Name = "buttonRegister";
+            this.buttonRegister.Size = new System.Drawing.Size(75, 23);
+            this.buttonRegister.TabIndex = 1;
+            this.buttonRegister.Text = "注册";
+            this.buttonRegister.UseVisualStyleBackColor = true;
+            // 
+            // buttonForget
+            // 
+            this.buttonForget.Location = new System.Drawing.Point(391, 454);
+            this.buttonForget.Name = "buttonForget";
+            this.buttonForget.Size = new System.Drawing.Size(75, 23);
+            this.buttonForget.TabIndex = 2;
+            this.buttonForget.Text = "忘记密码";
+            this.buttonForget.UseVisualStyleBackColor = true;
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Location = new System.Drawing.Point(107, 78);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(52, 15);
+            this.usernameLabel.TabIndex = 1;
+            this.usernameLabel.Text = "用户名";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(107, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "密码";
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(199, 75);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(160, 25);
+            this.textBoxUsername.TabIndex = 3;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(199, 158);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '*';
+            this.textBoxPassword.Size = new System.Drawing.Size(160, 25);
+            this.textBoxPassword.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 489);
+            this.Controls.Add(this.buttonForget);
+            this.Controls.Add(this.buttonRegister);
+            this.Controls.Add(this.panelLogin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panelLogin.ResumeLayout(false);
+            this.panelLogin.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelLogin;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Button buttonRegister;
+        private System.Windows.Forms.Button buttonForget;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label usernameLabel;
+    }
+}
+
