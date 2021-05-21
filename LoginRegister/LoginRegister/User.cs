273672b53b9c247c57c207966f8d9129539d1ead
wzk <1403235458@qq.com>
2021-05-21 09:32:17 +0800

    登陆注册、忘记密码

---
+++ LoginRegister/LoginRegister/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginRegister
+{
+    class User
+    {
+        public int UserId { get; set; }
+        public String Username { get; set; }
+        public String PhoneNumber { get; set; }
+        public String Password { get; set; }
+        public String Email { get; set; }
+        public Image Image { get; set; }
+
+        public User(string username, string password, string email)
+        {
+            Username = username;
+            Password = password;
+            Email = email;
+        }
+    }
+}
