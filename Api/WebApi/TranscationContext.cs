cd184be8afe1bfa89e7346e598f0ebfd72d162b6
guozhan <1054468404@qq.com>
2021-06-03 22:09:09 +0800

    登录注册APi，webSocket服务器端

---
+++ Api/WebApi/TranscationContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Models;
+
+namespace WebApi
+{
+    public class TranscationContext : DbContext
+    {
+        public TranscationContext(DbContextOptions<TranscationContext> options)
+           : base(options)
+        {
+            this.Database.EnsureCreated(); //自动建库建表
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
