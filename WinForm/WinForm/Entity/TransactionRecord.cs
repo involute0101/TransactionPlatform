fde573076a530fb26986af8049c9f54bc57091c0
wzk <1403235458@qq.com>
2021-06-17 20:17:32 +0800

    添加购买商品功能，往数据库中添加交易记录。更改属性大小写

---
+++ WinForm/WinForm/Entity/TransactionRecord.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WinForm.EntityClass
+{
+    public class TransactionRecord
+    {
+        public int TransactionRecordId { get; set; }
+
+        public int GoodId { get; set; }//商品
+
+        public int BuyerId { get; set; }//买家
+
+        public int SalerId { get; set; }//卖家
+
+        public DateTime Time { get; set; }//完成交易时间
+
+        public TransactionRecord(int goodId, int buyerId, int salerId, DateTime time)
+        {
+            GoodId = goodId;
+            BuyerId = buyerId;
+            SalerId = salerId;
+            Time = time;
+        }
+    }
+}
---
+++ WinForm/WinForm/Service/TransactionService.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Models;
+using WinForm.EntityClass;
+using WinForm.Tool;
+
+namespace WinForm.Service
+{
+    class TransactionService
+    {
+        private static String serverAddress = "http://localhost:5001/"; //服务器地址
+        //根据id查询交易记录
+        public static List<Good> GetAllRecordsById(int id)
+        {
+            string baseUrl = serverAddress + "TransactionRecord/getTransactionRecordByBuyer?" +
+                "buyerId=" + id;
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var task = client.GetStringAsync(baseUrl);
+            //得到买家所有买的商品
+            List<Good> goods = JsonConvert.DeserializeObject<List<Good>>(task.Result);
+            foreach (var g in goods)
+            {
+                MemoryStream ms = new MemoryStream(g.ImageByte);
+
+                g.Image = ImageTool.ResizeImage(new Bitmap(ms), new Size(200, 200));
+                ms.Close();
+            }
+            return goods;
+        }
+
+        public static bool AddRecord(TransactionRecord record)
+        {
+            string baseUrl = serverAddress + "TransactionRecord/addTransactionRecord?";
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpContent content1 = new StringContent(JsonConvert.SerializeObject(record),
+                Encoding.UTF8, "application/json");
+            var task = client.PostAsync(baseUrl, content1);
+            task.Wait();
+            if (task.Result == null)
+                return false;
+            else
+                return true;
+        }
+
+
+    }
+}
